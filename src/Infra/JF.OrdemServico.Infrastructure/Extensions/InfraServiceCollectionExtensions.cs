ef97d4fa0ec1f35ef8a186fd6d2c0a732b41b68f
Fernando Oliveira <85460491+FernandoOliveira1218@users.noreply.github.com>
2025-07-14 12:23:44 -0300

    feat(infra): adiciona e configura DbContext com PostgreSQL
    - Criado OrdemServicoDbContext com DbSet para Chamado e Cliente
    - Registrado DbContext via AddInfra utilizando UseNpgsql
    - Adicionado suporte à injeção de dependência da Infra com IConfiguration
    - Pronto para geração de migrations e integração com PostgreSQL

--- src/Infra/JF.OrdemServico.Infrastructure/Extensions/InfraServiceCollectionExtensions.cs
+++ src/Infra/JF.OrdemServico.Infrastructure/Extensions/InfraServiceCollectionExtensions.cs
@@ -1,19 +1,24 @@
 ﻿using JF.OrdemServico.Domain.Interfaces.Repositories;
 using JF.OrdemServico.Infra.Data.Common;
+using JF.OrdemServico.Infra.Data.Context;
 using JF.OrdemServico.Infra.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace JF.OrdemServico.Infra.Extensions;
 
 public static class InfraServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfra(this IServiceCollection services)
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
     {
+        // Configuração do DbContext com PostgreSQL
+        services.AddDbContext<OrdemServicoDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        // Repositórios
         services.AddScoped(typeof(IRepositoryBase<>), typeof(RepositoryBase<>));
         services.AddScoped<IChamadoRepository, ChamadoRepository>();
         services.AddScoped<IClienteRepository, ClienteRepository>();
-
-        // Adicione contextos e configurações de banco aqui
 
         return services;
     }
